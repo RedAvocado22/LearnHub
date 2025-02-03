ff33f8d379e8a4311f5f955e1f3991071ec0a3d0
LamCao1130 <caohailam3011@gmail.com>
2025-02-04 02:39:13 +0700

    feat: contact us

---
+++ learnhub-api/src/main/java/com/learnhub/user/ContactController.java
@@ -0,0 +1,27 @@
+package com.learnhub.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class ContactController {
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @PostMapping("/contact")
+    public ResponseEntity<String> contactUs(@RequestBody ContactRequest contact) {
+        contactService.saveContact(contact);
+        return ResponseEntity.ok("Success");
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Success");
+    }
+}
