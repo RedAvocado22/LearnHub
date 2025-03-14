b5d3fbd96ac0ac6908a96d3218740a4ed92edb2a
RedAvocado <minhcuong2922004@gmail.com>
2025-03-14 15:27:04 +0700

    chorus: format code

--- learnhub-api/src/main/java/com/learnhub/user/UserDetailsServiceImpl.java
+++ learnhub-api/src/main/java/com/learnhub/user/UserDetailsServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -18,7 +19,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
-            .orElseThrow(() -> new UsernameNotFoundException(
+                .orElseThrow(() -> new UsernameNotFoundException(
                         String.format("User with username: %s not found", username)));
     }
 }
