76e0b5796cb5fcac09d4edfc9a234d392c6314e9
RedAvocado <minhcuong2922004@gmail.com>
2025-03-23 17:04:37 +0700

    chores: add mock course manager data

--- learnhub-api/src/main/resources/db/insert.sql
+++ learnhub-api/src/main/resources/db/insert.sql
@@ -8,7 +8,9 @@ values
 ('doanquocbaooooooo@gmail.com', 'BaoS', 'Doan Quoc', '$2a$10$BHIFmEpxSFaUGlcsCp5F5usmJirNKINm6eRkhy/rr8YgcO4QcED/m', 'STUDENT',  'ACTIVE', now()), -- ABC@456
 ('doanbao2506@gmail.com', 'BaoT1', 'Doan Quoc', '$2a$10$WG7RjXtZRyBjj6388I8fROrCr5AFWp2wnUXUFLrDYoazEDxZ4kWlC', 'TEACHER',  'ACTIVE', now()), -- ABC@789
 ('doanqbao2506@gmail.com', 'BaoT2', 'Doan Quoc', '$2a$10$m7C2R4n7BAxTGlZ1obEM0uH2iCBn.KwpE8s8GEY9QTeKZ62e1eibm', 'TEACHER',  'ACTIVE', now()), -- ABC@012
-('minhcuong2922004@gmail.com', 'CuongCM', 'Minh Cuong', '$2a$10$m7C2R4n7BAxTGlZ1obEM0uH2iCBn.KwpE8s8GEY9QTeKZ62e1eibm', 'COURSE_MANAGER',  'ACTIVE', now()); -- ABC@012
+('minhcuong2922004@gmail.com', 'CuongCM', 'Minh Cuong', '$2a$10$m7C2R4n7BAxTGlZ1obEM0uH2iCBn.KwpE8s8GEY9QTeKZ62e1eibm', 'COURSE_MANAGER',  'ACTIVE', now()), -- ABC@012
+('coursemanager@gmail.com', 'Course', 'Manager', '$2a$10$m7C2R4n7BAxTGlZ1obEM0uH2iCBn.KwpE8s8GEY9QTeKZ62e1eibm', 'COURSE_MANAGER',  'ACTIVE', now()); -- ABC@012
+
 insert into student_profile(account_id, school, `type`)
 values
 (2, 'FPT University', 'GRADE11');
@@ -19,7 +21,9 @@ values
 (4, 'Literature 11', '0123456789', 'https://google.com', 'ABC School', 'Ho Chi Minh city', null);
 
 insert into manager_profile(account_id, department)
-values (5, "Math");
+values
+(5, "Math"),
+(6, "Course Manager Department");
 
 insert into category(`name`)
 values ('Calculus'), ('Algebra'), ('Vietnam\'s Literature'), ('Foreign Literature');
